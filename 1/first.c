28e36f61860810e658b28b956799429801daf974
Mahmood Seddiqui <seddiquimahmood@gmail.com>
2025-08-05 16:57:48 +0430

    first change

--- 1/first.c
+++ 1/first.c
@@ -5,13 +5,12 @@
 typedef char* string;
     
 int main () {
-    string str1 = "Hello, ";
+    string str1 = "Hello,";
     string str2 = "World!";
     string str3 = "Hello, World!";
 
-    printf("str1: %s\n", str1);
-    printf("str2 yes: %s\n", str2);
-    printf("str3 are the best: %s\n", str3);
+    printf("str1 and str2: %s %s\n", str1, str2);
+    printf("str3: %s\n", str3);
     
     return 0;
 }
