932c7ccb7dec9e911c4e44ce36517382378354bf
Mahmood Seddiqui <seddiquimahmood@gmail.com>
2025-08-07 16:54:27 +0430

    update

--- 1/first.c
+++ 1/first.c
@@ -1,16 +1,6 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
 
-typedef char* string;
-    
-int main () {
-    string str1 = "Hello,";
-    string str2 = "World!";
-    string str3 = "Hello, World!";
-
-    printf("str1 and str2: %s %s\n", str1, str2);
-    printf("str3: %s\n", str3);
-    
+int main() {
+    printf("Hello, World!\n");
     return 0;
 }
