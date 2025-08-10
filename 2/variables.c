4cd519a3fe4248cdb158feb0c59bc2c81750601b
Mahmood Seddiqui <seddiquimahmood@gmail.com>
2025-08-10 05:55:11 +0430

    practice

--- 2/variables.c
+++ 2/variables.c
@@ -1,14 +1,42 @@
 #include<stdio.h>
+
+typedef char* string;
+
+typedef struct {
+  string name;
+  int age;
+}Person;
+
 int main() {
-  int a;
-  printf("Enter a: ");
-  scanf("%d", &a);
-  for(int i = 0; i < a; i++) {
-    for(int j = 0; j < i; j++) {
-      printf("*");
-    }
-    printf("\n");
-  }
+  // Person person;
+  // person.name = "John Doe";
+  // person.age = 30;
+  // printf("Name: %s\n", person.name);
+  // printf("Age: %d\n", person.age);
+  // int a;
+  // printf("Enter a: ");
+  // scanf("%d", &a);
+  // for(int i = 1; i <= a; i++) {
+  //   for(int j = a; j >= i; j--) {
+  //     printf("*");
+  //   }
+  //   printf("\n");
+  // }
+  // for (int i = 5; i > 0; i--)
+  // {
+  //   printf("*\n");
+  // }
+  
+  // printf("    *\n");
+  // printf("   **\n");
+  // printf("  ***\n");
+  // printf(" ****\n");
+  // printf("*****\n");
+
+  // char name[50];
+  // printf("Enter your name: ");
+  // scanf("%s", name); // Using %ms to allocate memory for the string
+  // printf("Hello, %s!\n", name);
 
   return 0;
 }
